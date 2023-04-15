c2baa414b87610c570f0ef0bfb8dbb5d9e29119c
Kevin <eisermanka@gmail.com>
2023-04-15 16:25:06 -0400

    Removed array variable and fixed issue where number wasn't being operated on after selecting a second operator

--- script.js
+++ script.js
@@ -2,40 +2,50 @@ const numberButtons = document.querySelectorAll(".number-btn");
 const displayCurrent = document.querySelector(".display-current");
 const displayLast = document.querySelector(".display-last");
 const clearButton = document.querySelector(".clear-btn");
+const equalButton = document.querySelector(".equal-btn");
 const operatorButtons = document.querySelectorAll(".operator-btn");
-const number = [];
 
-let result = 0;
+let current = 0;
 let placeholder = 0;
 let operator;
 
-
 clearButton.addEventListener("click", clear);
+
+console.log(operate("-", 6, 10));
 
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-        if (result === 0) {
-            result = result.toString().slice(1);
-        }    
-        result += button.textContent;
-        displayCurrent.textContent = result;
+        if (current === 0) {
+            current = current.toString().slice(1);
+        }
+        current += button.textContent;
+        displayCurrent.textContent = current;
     });
 });
 
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        if (placeholder === 0) {
-            placeholder = result;
-            operator = button.textContent;
-            displayLast.textContent = placeholder + " " + operator;
-        } else {
-            result = operate(operator, placeholder, result);
-            displayCurrent.textContent = result;
+        if (placeholder != 0) {
+            current = operate(operator, current, placeholder);
+            displayCurrent.textContent = current;
         }
+        placeholder = current;
+        operator = button.textContent;
+        current = 0;
+        displayCurrent.textContent = placeholder;
+        displayLast.textContent = placeholder + " " + operator;
     });
 });
 
+equalButton.addEventListener("click", () => {
+    current = operate(operator, current, placeholder);
+    displayCurrent.textContent = current;
+    displayLast.textContent = placeholder + " " + operator;
+});
+
 function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
     switch (operator) {
         case "+":
           return add(num1, num2);
@@ -67,9 +77,9 @@ function divide(num1, num2) {
 }
 
 function clear() {
-    result = 0;
+    current = 0;
     placeholder = 0;
     placeholder = placeholder.toString().slice(1);
     displayLast.textContent = placeholder;
-    displayCurrent.textContent = result;
+    displayCurrent.textContent = current;
 }
